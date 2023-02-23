33ab02e3e2bfd173e4e472b6428fe4dff3076d88
Carokil1 <chucholimosr@gmail.com>
2023-02-22 16:30:07 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,15 +11,11 @@ void times_table(void)
 {
 	int i, j;
 
-	printf("  0  1  2  3  4  5  6  7  8  9\n");
-	printf("------------------------------\n");
 	for (i = 0; i <= 9; i++)
 	{
-		printf("%2d|", i);
 		for (j = 0; j <= 9; j++)
 		{
-			printf("%2d ", i * j);
+			printf("%2d%s", i * j, j == 9 ? "\n" : ", ");
 		}
-		printf("\n");
 	}
 }
