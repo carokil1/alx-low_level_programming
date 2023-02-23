162da547f22a9d2418d1e32c9fb024e3886573be
Carokil1 <chucholimosr@gmail.com>
2023-02-23 04:33:40 -0800

    task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
+/**
+ * print_fibonacci - prints 98 fibonacci numbers
+ *
+ * Return - always returns 0
+ */
 
 void print_fibonacci(int n)
 {
 	int prev = 1;
 	int curr = 2;
-	
+
 	printf("%d, %d", prev, curr);
-	
 	for (int i = 3; i <= n; i++)
 	{
-        int next = prev + curr;
-        printf(", %d", next);
-        prev = curr;
-        curr = next;
+		int next = prev + curr;
+
+		printf(", %d", next);
+		prev = curr;
+		curr = next;
 	}
 	printf("\n");
 }
 
-int main()
+int main(void)
 {
 	print_fibonacci(98);
-	return 0;
+	return (0);
 }
