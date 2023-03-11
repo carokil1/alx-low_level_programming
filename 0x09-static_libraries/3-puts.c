445229961570fa7d288f241e486175e5cd93131a
Carokil1 <chucholimosr@gmail.com>
2023-03-11 03:28:00 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _puts - prints a string to stdout
+ * @str: the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	/* code to print the string to stdout */
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
