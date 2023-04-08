173948cb20ab8730ed4ba9fd332c699a8acd9b16
carokil1 <chucholimosr@gmail.com>
2023-04-08 12:18:07 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,23 +2,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 #define BUFFER_SIZE 1024
 
+/**
+ * main - Copies the content of a file to another file
+ * @argc: The number of arguments
+ * @argv: The array of arguments
+ *
+ * Return: 0 on success, or an exit status on failure
+ */
 int main(int argc, char *argv[])
 {
-	int fd_from = open(argv[1], O_RDONLY);
+	int fd_from, fd_to, num_r, num_w;
+	char buffer[BUFFER_SIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	if (argc != 3 || fd_from == -1)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, argc != 3 ?
-				"Usage: cp file_from file_to\n" :
-				"Error: Can't read from file %s\n", argv[1]);
-		exit(argc != 3 ? 97 : 98);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	int fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -26,13 +38,10 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	char buffer[BUFFER_SIZE];
-	ssize_t num_read, num_written;
-
-	while ((num_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((num_r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		num_written = write(fd_to, buffer, num_read);
-		if (num_written == -1 || num_written != num_read)
+		num_w = write(fd_to, buffer, num_r);
+		if (num_w == -1 || num_w != num_r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
@@ -41,7 +50,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (num_read == -1)
+	if (num_r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
@@ -64,3 +73,4 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
